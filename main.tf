b4a77f0b32eca9c4ba0aa84bffaeb20f31d5be28
kp3073 <keyurpatel8288@gmail.com>
2024-11-12 23:03:41 -0500

    roboshop first commit

---
+++ main.tf
@@ -0,0 +1,58 @@
+variable "component" {
+  default = [
+    "catalogue",
+    "user",
+    "cart",
+    "shipping",
+    "payment",
+    "mongodb",
+    "mysql",
+    "rabbitmq",
+    "redis",
+    "dispatch",
+  ]
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+  }
+
+resource "aws_instance" "instance" {
+  count = length(var.component)
+  ami                     = data.aws_ami.ami.id
+  instance_type           = "t3.micro"
+  vpc_security_group_ids = ["sg-006eca25fc0b7619d"]
+  tags = {
+    Name = element(var.component, count.index)
+  }
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "aligntune.online"
+  private_zone = true
+}
+
+variable "comopnent" {
+  default = ""
+}
+resource "aws_route53_record" "record" {
+  count = length(var.component)
+  zone_id = data.aws_route53_zone.selected.id
+  name    = "${element(var.comopnent, count.index)} -dev"
+  type    = "A"
+  ttl     = 30
+  records = [element(aws_instance.instance.*.private_ip, count.index )]
+}
+
+resource "null_resource" "local" {
+  provisioner "remote-exec" {
+    connection {
+      host = element(aws_instance.instance.*.private_ip, count.index)
+      user     = "root"
+      password = "DevOps321"
+    }
+    inline = "set-hostname ${element(var.comopnent, count.index)}"
+  }
+}
